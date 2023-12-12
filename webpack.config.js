e5e7a6a8dcdd4933b281f8967443ddcba7be6555
Joe Boyle <jboyle617@gmail.com>
2023-12-11 22:14:07 -0500

    remove extra loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,10 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  resolveLoader: {
-    alias: {
-      "react-loader": path.resolve(__dirname, "src", "react-loader.js"),
-    },
-  },
   plugins: [
     new HtmlWebpackPlugin({
       inject: false,
-      template: "react-loader!./src/app.js",
+      template: "./src/index.html",
     }),
   ],
 };
